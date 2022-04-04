e2f0026d122762ff65b282cf9516f35846c46a55
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-04-04 15:55:30 -0400

    adding in delay support

--- waspc/data/Generator/templates/server/src/jobs/_jobs.js
+++ waspc/data/Generator/templates/server/src/jobs/_jobs.js
@@ -1,13 +1,38 @@
 {{={= =}=}}
 {=& jobFnImportStatement =}
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 export default class {= jobName =} {
-  static performAsync(args) {
-    const fn = {= jobFnName =}(args)
-    return {
-      result: async function() {
-        return await fn
+  constructor(delayMs) {
+    this.delayMs = delayMs || 0
+  }
+
+  performAsync(args) {
+    async function fn(delayMs) {
+      if (delayMs > 0) {
+        await sleep(delayMs)
       }
+      return {= jobFnName =}(args)
     }
+
+    const res = fn(this.delayMs)
+
+    return {
+      result: async () => await res
+    }
+  }
+
+  static delay(ms) {
+    return new {= jobName =}(ms)
+  }
+
+  static performAsync(args) {
+    const job = new {= jobName =}(0)
+    return job.performAsync(args)
   }
 }
